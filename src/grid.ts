bd319814cd896044cab662983b1d4b3cdc7249fb
Michael Martis <michaeljmartis@gmail.com>
2023-02-02 18:29:24 +1100

    Added canOccupy

--- src/grid.ts
+++ src/grid.ts
@@ -1,3 +1,8 @@
+// For cleaner for loops.
+function range(start: number, end: number) {
+  return [...Array(1 + end - start).keys()].map(v => start + v);
+}
+
 class Vec2D {
   constructor(readonly x: number, readonly y: number) {}
 
@@ -6,8 +11,11 @@ class Vec2D {
   }
 }
 
-// Strong types for different vector use cases.
 class Point extends Vec2D {}
+
+class Rect {
+  constructor(readonly tl: Point, readonly br: Point) {}
+}
 
 // The steps that can be taken from one cell to an adjacent one.
 const DIRS: Vec2D[] = [
@@ -18,9 +26,6 @@ const DIRS: Vec2D[] = [
 ];
 
 class Grid {
-  // False if partitions need recalculating.
-  private fresh: boolean;
-
   // True if the cell is impassable.
   private blocked: boolean[][];
 
@@ -28,7 +33,6 @@ class Grid {
   private part: number[][];
 
   constructor(private readonly w: number, private readonly h: number) {
-    this.fresh = true;
     this.blocked = new Array(h).map(() => new Array(w).fill(false));
     this.part = new Array(h).map(() => new Array(w).fill(1));
   }
@@ -36,9 +40,6 @@ class Grid {
   // Marks all cells in the line between p1 and p2 blocked. Uses the Bresenham
   // line drawing algorithm.
   obstructLine({x: x1, y: y1}: Point, {x: x2, y: y2}: Point): void {
-    // Force recalculation of next fill.
-    this.fresh = true;
-
     const dx: number = Math.abs(x2 - x1);
     const sx: number = x1 < x2 ? 1 : -1;
     const dy: number = -Math.abs(y2 - y1);
@@ -68,23 +69,32 @@ class Grid {
     }
   }
 
+  // Returns true if the given cell is obstructed.
   isBlocked({x: x, y: y}: Point): boolean {
     if (x < 0 || x >= this.w || y < 0 || y >= this.h || this.blocked[y][x])
       return true;
     return false;
   }
 
-  calculatePartitions() {
-    if (this.fresh) return;
-    this.fresh = true;
+  // Returns true if there are no obstructions in the given rectangle.
+  canOccupy({tl: {x: x1, y: y1}, br: {x: x2, y: y2}}: Rect) {
+    for (const y of range(y1, y2 + 1)) {
+      for (const x of range(x1, x2 + 1)) {
+        if (this.isBlocked(new Point(x, y))) return false;
+      }
+    }
 
+    return true;
+  }
+
+  calculatePartitions() {
     // Reset partitions, using 0 to denote "unvisited".
     this.part = new Array(this.h).map(() => new Array(this.w).fill(0));
     let curPart = 1;
 
     // Step through every cell.
-    for (const oY of new Array(this.h).keys()) {
-      for (const oX of new Array(this.w).keys()) {
+    for (const oY of range(0, this.h)) {
+      for (const oX of range(0, this.w)) {
         // Already seen.
         if (this.part[oY][oX] !== 0) continue;
 
